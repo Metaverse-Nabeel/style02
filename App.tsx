fb2676bee1c9465caf298ec27843d0da2158880c
Nabeel <nabeelahmed3129@gmail.com>
2024-04-22 22:44:52 +0500

    Elevated card

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
 import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
 
 function App() {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCards />
+        <ElevatedCards />
       </ScrollView>
     </SafeAreaView>
   );
